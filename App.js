0d156747d90beb3d0aba6f86fe028252055f1009
youssef <ysf.askri@gmail.com>
2024-09-16 15:37:41 +0200

    init

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 function App() {
     return (
         <div>
-            <h1>Hello, World! Youssef</h1>
+            <h1>Hello, World! Iyed</h1>
             <p>Welcome to my React app deployed with ArgoCD!</p>
         </div>
     );
